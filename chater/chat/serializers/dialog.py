87bb34cc4d6e1ece9a08bd46b0e7cd13c16ae49d
amaozhao <amaozhao@gmail.com>
2023-04-09 23:27:20 +0800

    [WIP]

--- chater/chat/serializers/dialog.py
+++ chater/chat/serializers/dialog.py
@@ -1,14 +1,24 @@
+from django.urls import reverse
 from rest_framework import serializers
 
 from ..models import Dialog, Log
 
 
+class LogsUrlField(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def get_attribute(self, instance):
+        return reverse("log-list", args=[instance.id])
+
+
 class DialogSerializer(serializers.ModelSerializer):
     deleted = serializers.BooleanField(read_only=True)
+    logs_url = LogsUrlField(read_only=True)
 
     class Meta:
         model = Dialog
-        fields = ["id", "title", "created", "updated", "deleted"]
+        fields = ["id", "title", "created", "updated", "deleted", "logs_url"]
 
 
 class LogSerializer(serializers.ModelSerializer):
